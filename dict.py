28de5e226261919d2f82c4a989a1a5c2a32b594d
Angel Ortiz <aortizmedina@csumb.edu>
2017-12-04 11:18:51 -0800

    first commit

---
+++ dict.py
@@ -0,0 +1,14 @@
+image_info = [
+    {
+        "id" : "bit",
+        "title" : "bit building",
+    },
+    {
+        "id" : "round",
+        "title" : "Library Roundabout",
+    },
+    {
+        "id" : "csumb",
+        "title" : "Monte Rey",
+    }
+    ]
